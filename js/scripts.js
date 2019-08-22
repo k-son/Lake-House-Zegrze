8c1603fa4f5bcd7742ad708bb7df313f4c16dc67
Jacek Jarczok <kson.eu@gmail.com>
2019-08-22 08:55:12 +0200

    Develop scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+///// HAMBURGER MENU
+
+const hamburgerIcon = document.getElementById('hamburgerIcon');
+const modalMenu = document.getElementById('modalMenu');
+const closeIcon = document.getElementById('closeIcon'); 
+const modalMenuLinks = document.querySelectorAll('.hamburger__link');
+
+hamburgerIcon.addEventListener('click', () => {
+  hamburgerIcon.style.visibility = 'hidden';
+  modalMenu.style.display = 'block';
+})
+
+function closeModalMenu() {
+  modalMenu.style.display = 'none';
+  hamburgerIcon.style.visibility = 'visible';
+}
+
+closeIcon.addEventListener('click', closeModalMenu);
+
+let i = 0;
+while (i < modalMenuLinks.length) {
+  modalMenuLinks[i].addEventListener('click', closeModalMenu);
+  i++;
+}
+
+
+
