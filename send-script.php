e7559fd3d172072932445f3096fd2847c79575b7
Jacek Jarczok <kson.eu@gmail.com>
2019-09-11 12:33:20 +0200

    Put back $email in from

--- send-script.php
+++ send-script.php
@@ -33,7 +33,7 @@ if ( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
 	} else {
 		$headers = 'MIME-Version: 1.0' . "\r\n";
 		$headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
-		$headers .= 'From: ' . $name . "\r\n";
+		$headers .= 'From: ' . $email . "\r\n";
 		$headers .= 'Reply-to: ' . $email;
 		$message = "
 			<html>
